dee347d867dd0bbb0ba512c5cd6f6815955333a5
Matthew Davies <daviesgeek@icloud.com>
2015-12-10 15:55:07 -0800

    Update example package version

--- example/Package.swift
+++ example/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "swiftrouter-example",
   dependencies: [
-    .Package(url: "https://github.com/daviesgeek/swiftrouter.git", Version(0, 0, 2))
+    .Package(url: "https://github.com/daviesgeek/swiftrouter.git", Version(0, 0, 3))
   ]
 )
